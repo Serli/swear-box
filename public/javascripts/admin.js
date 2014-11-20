2dc140d5e0a7d22edaf033e43114f49218899dc6
GBerge <geoffrey.berge@live.fr>
2014-11-20 19:56:08 +0100

    Fonctionnalité : Supprimer un membre
    - ng-click sur le bouton "supprimer"
    - fonction dans le contrôleur admin pour supprimer un membre via
    $http.post

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -15,27 +15,49 @@ adminApp.controller('listCtrl', ['$scope', '$http', function($scope, $http){
 		$scope.members.push($scope.newMember);	
 
 		var dataObj = {
-			name : $scope.newMember.name,
-			firstname : $scope.newMember.firstname
+				name : $scope.newMember.name,
+				firstname : $scope.newMember.firstname
 		};			
 
-  	$http.post('/addPerson', dataObj)
+		$http.post('/addPerson', dataObj)
 		.success(function(data, status, headers, config){
 			console.log("suc");
 			$http.get('/person')
-				.success(function(data, status, headers, config){
-					$scope.members = data;
-				})
-				.error(function(data, status, headers, config){
-				});
+			.success(function(data, status, headers, config){
+				$scope.members = data;
+			})
+			.error(function(data, status, headers, config){
+			});
 		})
 		.error(function(data, status, headers, config){
 			console.log("err");
 		});
 
-  	$scope.newMember.name= '';
-  	$scope.newMember.firstname= '';
+		$scope.newMember.name= '';
+		$scope.newMember.firstname= '';
 
-  }
+	}
+
+	$scope.deleteMember = function (idt) {
+		var dataObj = {
+				id : idt
+		};
+
+		$http.post('/deletePerson', dataObj)
+		.success(function(data, status, headers, config){
+			console.log("suc");
+			$http.get('/person')
+			.success(function(data, status, headers, config){
+				$scope.members = data;
+			})
+			.error(function(data, status, headers, config){
+			});
+		})
+		.error(function(data, status, headers, config){
+			console.log("err");
+		});
+
+	};
+
 }]);
 
