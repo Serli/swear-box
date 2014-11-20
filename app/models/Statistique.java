3d5f2b865002edc1e5df35986689666797b0014a
safia <safiaboukhdimi@gmail.com>
2014-11-20 09:22:09 +0100

    Ajout import

--- app/models/Statistique.java
+++ app/models/Statistique.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
