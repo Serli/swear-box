cead005c6e1d19422c1a3bcf46389a29dee71986
sfabien <simon-fabien@hotmail.fr>
2014-12-16 11:00:41 +0100

    Changer le montant de la dette par défaut
    Classe métier
    Controller
    test unitaire

--- app/dao/ConsumerDAO.java
+++ app/dao/ConsumerDAO.java
@@ -26,4 +26,20 @@ public final class ConsumerDAO {
             JPA.em().persist(u);
         }
     }
+    
+    /**
+     * Modifie le montant de la penalité.
+     * @param email email de l'utilisateur ( clé primaire de la table Utilisateur )
+     */
+    public static void updateAmount(String email, int vAmount) {
+        //Recuperation de l'utilisateur
+        Consumer u = JPA.em().find(Consumer.class, email);
+        
+        //Si l'utilisateur n'existe pas il est ajouté
+        if (u != null) {
+            u.setAmount(vAmount);
+        }
+        
+        JPA.em().flush();
+    }
 }
---
+++ test/unit/UpadteAmountUserTest.java
@@ -0,0 +1,50 @@
+package unit;
+import models.Consumer;
+import play.db.jpa.JPA;
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+import org.junit.*;
+
+import dao.ConsumerDAO;
+
+
+/**
+ * Test la classe AddUser
+ * @author Geoffrey
+ *
+ */
+public class UpadteAmountUserTest {
+
+    /**
+     * Test l'ajout d'un utilisateur dans la base de données 
+     */
+    @Test
+    public void addUser() {
+        running(fakeApplication(inMemoryDatabase()), new Runnable()
+        {
+            public void run()
+            {
+                JPA.withTransaction(new play.libs.F.Callback0()
+                {
+                    public void invoke()
+                    {
+                        //Ajout d'un utilisateur à partir d'un email
+                        String email = "test@gmail.com";
+                        ConsumerDAO.add(email);
+
+                        ConsumerDAO.updateAmount(email, 20);
+                        
+                        //Recherche de l'utilisateur dans la base de données
+                        Consumer u = JPA.em().find(Consumer.class, email);
+                        assertThat(u.getAmount()==20);
+
+                        JPA.em().remove(u);
+                    }
+                });
+            }
+        });
+    }
+}
+
+
