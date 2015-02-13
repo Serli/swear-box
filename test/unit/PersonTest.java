7ac8fc36be23a0403a8617d72c6633cfbb6d08c8
sfabien <simon-fabien@hotmail.fr>
2015-02-13 11:51:35 +0100

    Test mongodb debut

--- test/unit/PersonTest.java
+++ test/unit/PersonTest.java
@@ -3,13 +3,20 @@ package unit;
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.inMemoryDatabase;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.Query;
 
 import models.Consumer;
 import models.Person;
+import models.Statistics;
+import net.vz.mongodb.jackson.DBQuery;
+import net.vz.mongodb.jackson.DBRef;
+import net.vz.mongodb.jackson.JacksonDBCollection;
 
+import org.bson.types.ObjectId;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,6 +24,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import play.db.jpa.JPA;
+import play.modules.mongodb.jackson.MongoDB;
 import play.test.FakeApplication;
 import play.test.Helpers;
 import dao.ConsumerDAO;
@@ -30,20 +38,24 @@ import dao.PersonDAOImpl;
  */
 public class PersonTest{
 
-    private PersonDAO personDAO = new PersonDAOImpl();
+	private PersonDAO personDAO = new PersonDAOImpl();
     private ConsumerDAO consumerDAO = new ConsumerDAOImpl();
     private static FakeApplication app;
-
+    private static JacksonDBCollection<Consumer, String> consumers ;
+    private static JacksonDBCollection<Person, String> people;
+	private static JacksonDBCollection<Statistics, String> statistics;
+	
     @BeforeClass
     public static void startApp() {
-        app = Helpers.fakeApplication(inMemoryDatabase());
+    	Map<String, String> config = new HashMap<String, String>();
+        config.put("ehcacheplugin", "disabled");
+        config.put("mongodbJacksonMapperCloseOnStop", "disabled");
+        app = Helpers.fakeApplication(config);
         Helpers.start(app);
-        JPA.bindForCurrentThread( JPA.em("default"));
-    }
-
-    @Before
-    public void setUp() {
-        JPA.em().getTransaction().begin();
+        consumers = MongoDB.getCollection("Consumer", Consumer.class, String.class);
+    	people = MongoDB.getCollection("Person", Person.class, String.class);
+    	statistics = MongoDB.getCollection("Statistics", Statistics.class, String.class);
+        
     }
 
     /**
@@ -51,21 +63,21 @@ public class PersonTest{
      */
     @Test
     public void addPerson() {
-        Person p =new Person("Toto", "Titi",0,"yolo");
+        Person p =new Person(ObjectId.get().toString(),"Toto", "Titi",0,"yolo");
         Consumer u= new Consumer("email@email",100);
 
-        JPA.em().persist(u);
+        consumers.insert(u);
 
         personDAO.add(p,u.getEmail());
 
         //Get the person from the DB
-        Person pbd= (Person)JPA.em().createQuery("Select p FROM Person p WHERE p.name='Toto'").getSingleResult();
-
+        Person pbd=people.findOne(DBQuery.is("name", "Toto"));
+        
         //Check if the user has the person in his list
         assertThat(pbd).isNotEqualTo(null);
 
-        JPA.em().remove(u);
-        JPA.em().remove(pbd);
+        consumers.remove(u);
+        people.remove(pbd);
     }
 
     /**
@@ -73,27 +85,27 @@ public class PersonTest{
      */
     @Test
     public void deletePerson() {
-        Person p =new Person("Suppr-Toto", "Suppr-Titi",0,"yolo");
+        Person p =new Person(ObjectId.get().toString(),"Suppr-Toto", "Suppr-Titi",0,"yolo");
         Consumer u1= new Consumer("Suppr-email1@email",100);
 
         //Recording the users
-        JPA.em().persist(u1);
+        consumers.insert(u1);
 
         //Add the person and link it to an other user
         personDAO.add(p,u1.getEmail());
-        Person pbd= (Person)JPA.em().createQuery("Select p FROM Person p WHERE p.name='Suppr-Toto'").getSingleResult();
+        Person pbd= people.findOne(DBQuery.is("name", "Suppr-Toto"));
 
-        JPA.em().flush();
+        consumers.updateById("Suppr-email1@email", u1);
 
         //Delete the person for the two users
         personDAO.delete(pbd.getIdPerson(),"Suppr-email1@email");
 
         //Test if the person doesn't exist anymore
-        List<Person> lp= JPA.em().createQuery("Select p FROM Person p WHERE p.name='Suppr-Toto'",Person.class).getResultList();
+        List<Person> lp= people.find(DBQuery.in("name", "Suppr-Toto")).toArray();
         assertThat(lp).isEmpty();
 
         //Clean
-        JPA.em().remove(u1);
+        consumers.remove(u1);
     }
 
     /**
@@ -101,29 +113,28 @@ public class PersonTest{
      */
     @Test
     public void debtPerson() {
-        Person p =new Person("debt-Toto", "debt-Titi",10,"yolo");
+        Person p =new Person(ObjectId.get().toString(),"debt-Toto", "debt-Titi",10,"yolo");
         Consumer u1= new Consumer("debt-email1@email",100);
 
         //Recording users
-        JPA.em().persist(u1);
+        consumers.insert(u1);
 
         //Add the person and link it to an other user
         personDAO.add(p,u1.getEmail());
-        Person pbd= (Person)JPA.em().createQuery("Select p FROM Person p WHERE p.name='debt-Toto'").getSingleResult();
+        Person pbd=people.findOne(DBQuery.is("name", "debt-Toto"));
 
-        JPA.em().flush();
+        consumers.updateById("debt-email1@email", u1);
 
         //Discharge the person for the two users
         personDAO.discharge(pbd.getIdPerson(),"debt-email1@email");
 
         //Test if the debt is equals to 0
-        Query query = JPA.em().createQuery("Select p FROM Person p WHERE p.name='debt-Toto'");
-        Person pdebt = (Person) query.getSingleResult();
+        Person pdebt = people.findOne(DBQuery.is("name", "debt-Toto"));
         assertThat(pdebt.getDebt()==0);
 
         //Clean
-        JPA.em().remove(p);
-        JPA.em().remove(u1);
+        people.remove(p);
+        consumers.remove(u1);
     }
 
     /**
@@ -131,29 +142,30 @@ public class PersonTest{
      */
     @Test
     public void increaseDebtPerson() {
-        Person p =new Person("increase-Toto", "increase-Titi",0,"yolo");
+        Person p =new Person(ObjectId.get().toString(),"increase-Toto", "increase-Titi",0,"yolo");
         Consumer u1= new Consumer("increase-email1@email",100);
 
         //Recording users
-        JPA.em().persist(u1);
+        consumers.insert(u1);
 
         //Add the person and link it to an other user
         personDAO.add(p,u1.getEmail());
-        Person pbd= (Person)JPA.em().createQuery("Select p FROM Person p WHERE p.name='increase-Toto'").getSingleResult();
+        Person pbd= people.findOne(DBQuery.is("name", "increase-Toto"));
 
-        JPA.em().flush();
+        consumers.updateById("increase-email1@email", u1);
 
         //Increase the debt for the person
         personDAO.incrementDebt(pbd.getIdPerson(),"increase-email1@email");
+        
+        people.updateById(pbd.getIdPerson(), pbd);
 
         //Test if the debt is increased by the amount
-        Query query = JPA.em().createQuery("Select p FROM Person p WHERE p.name='increase-Toto'");
-        Person pdebt = (Person) query.getSingleResult();
+        Person pdebt = people.findOne(DBQuery.is("name", "increase-Toto"));
         assertThat(pdebt.getDebt()==50);
 
         //Clean
-        JPA.em().remove(u1);
-        JPA.em().remove(p);
+        consumers.remove(u1);
+        people.remove(p);
     }
 
     /**
@@ -164,28 +176,37 @@ public class PersonTest{
         // Add 2 users
         String emailU1 = "test1@email.com";
         Consumer u1 = new Consumer(emailU1, 100);
-        JPA.em().persist(u1);
+        consumers.insert(u1);
         Consumer u2 = new Consumer("test2@email.com", 100);
-        JPA.em().persist(u2);
+        consumers.insert(u2);
 
         // Add 3 persons
-        Person p1 = new Person("nom1", "prenom1", 1, "adr1");
-        Person p2 = new Person("nom2", "prenom2", 2, "adr2");
-        Person p3 = new Person("nom3", "prenom3", 3, "adr3");
-        JPA.em().persist(p1);
-        JPA.em().persist(p2);
-        JPA.em().persist(p3);
+        Person p1 = new Person(ObjectId.get().toString(),"nom1", "prenom1", 1, "adr1");
+        Person p2 = new Person(ObjectId.get().toString(),"nom2", "prenom2", 2, "adr2");
+        Person p3 = new Person(ObjectId.get().toString(),"nom3", "prenom3", 3, "adr3");
+        people.insert(p1);
+        people.insert(p2);
+        people.insert(p3);
 
         // Link persons to users
-        u1.setPerson(p1);
-        p1.setUser(u1);
-        u1.setPerson(p2);
-        p2.setUser(u1);
-        u2.setPerson(p3);
-        p3.setUser(u2);
+        DBRef<Person,String> pref = new DBRef<Person,String>(p1.getIdPerson(),Person.class);
+        DBRef<Consumer,String> uref = new DBRef<Consumer,String>(u1.getEmail(),Consumer.class);
+        u1.setPerson(pref);
+        p1.setUser(uref);
+        pref = new DBRef<Person,String>(p2.getIdPerson(),Person.class);
+        u1.setPerson(pref);
+        p2.setUser(uref);
+        pref = new DBRef<Person,String>(p3.getIdPerson(),Person.class);
+        uref = new DBRef<Consumer,String>(u1.getEmail(),Consumer.class);
+        u2.setPerson(pref);
+        p3.setUser(uref);
 
         //Synchronisation with the DB
-        JPA.em().flush();
+        consumers.updateById("test1@email", u1);
+        consumers.updateById("test2@email", u2);
+        people.updateById(p1.getIdPerson(), p1);
+        people.updateById(p2.getIdPerson(), p2);
+        people.updateById(p3.getIdPerson(), p3);
 
         //Get the list of the person link to u1
         List<Person> l = personDAO.listByUser(emailU1);
@@ -194,11 +215,11 @@ public class PersonTest{
         assertThat(l.get(0).getFirstname()).isEqualTo(p1.getFirstname());
         assertThat(l.get(1).getFirstname()).isEqualTo(p2.getFirstname());
 
-        JPA.em().remove(u1);
-        JPA.em().remove(u2);
-        JPA.em().remove(p1);
-        JPA.em().remove(p2);
-        JPA.em().remove(p3);
+        consumers.remove(u1);
+        consumers.remove(u2);
+        people.remove(p1);
+        people.remove(p2);
+        people.remove(p3);
     }
 
     /**
@@ -206,29 +227,30 @@ public class PersonTest{
      */
     @Test
     public void updateNameFirstnamePerson() {
-        Person p =new Person("update-Toto", "update-Titi",0,"yolo");
+        Person p =new Person(ObjectId.get().toString(),"update-Toto", "update-Titi",0,"yolo");
         Consumer u1= new Consumer("update-email1@email",100);
 
         //Recording users
-        JPA.em().persist(u1);
+        consumers.insert(u1);
 
         //Add the person and link it to an other user
         personDAO.add(p,u1.getEmail());
-        Person pbd= (Person)JPA.em().createQuery("Select p FROM Person p WHERE p.name='update-Toto'").getSingleResult();
+        Person pbd= people.findOne(DBQuery.is("name", "update-Toto"));
 
-        JPA.em().flush();
+        consumers.updateById("update-email1@email", u1);
 
         //Update the name and the firstname of the person
         personDAO.updateNameFirstname(pbd.getIdPerson(),"update-email1@email","toto","titi");
 
+        people.updateById(pbd.getIdPerson(), pbd);
+        
         //Test if the update worked
-        Query query = JPA.em().createQuery("Select p FROM Person p WHERE p.name='toto'");
-        Person pdebt = (Person) query.getSingleResult();
+        Person pdebt = people.findOne(DBQuery.is("name", "toto"));
         assertThat(pdebt.getName().equals("toto"));
 
         //Clean
-        JPA.em().remove(u1);
-        JPA.em().remove(p);
+        consumers.remove(u1);
+        people.remove(p);
     }
 
     /**
@@ -236,29 +258,29 @@ public class PersonTest{
      */
     @Test
     public void updatePicturePerson() {
-        Person p =new Person("updateP-Toto", "updateP-Titi",0,"yolo");
+        Person p =new Person(ObjectId.get().toString(),"updateP-Toto", "updateP-Titi",0,"yolo");
         Consumer u1= new Consumer("updateP-email1@email",100);
 
         //Recording users
-        JPA.em().persist(u1);
+        consumers.insert(u1);
 
         //Add the person and link it to an other user
         personDAO.add(p,u1.getEmail());
-        Person pbd= (Person)JPA.em().createQuery("Select p FROM Person p WHERE p.name='updateP-Toto'").getSingleResult();
+        Person pbd= people.findOne(DBQuery.is("name", "updateP-Toto"));
 
-        JPA.em().flush();
+        consumers.updateById("updateP-email1@email", u1);
 
         //Update the picture the person
         personDAO.updatePicture(pbd.getIdPerson(),"updateP-email1@email","toto");
-
+        people.updateById(pbd.getIdPerson(), pbd);
+        
         //Test the picture is updated
-        Query query = JPA.em().createQuery("Select p FROM Person p WHERE p.name='updateP-Toto'");
-        Person pdebt = (Person) query.getSingleResult();
+        Person pdebt = people.findOne(DBQuery.is("name", "updateP-Toto"));
         assertThat(pdebt.getPicture().equals("toto"));
 
         //Clean
-        JPA.em().remove(u1);
-        JPA.em().remove(p);
+        consumers.remove(u1);
+        people.remove(p);
     }
 
     /**
@@ -267,38 +289,35 @@ public class PersonTest{
     @Test
     public void linkUserToPerson() {
         //Creation of entities
-        Person p =new Person("Lier-Toto", "Lier-Titi",0,"yolo");
+        Person p =new Person(ObjectId.get().toString(),"Lier-Toto", "Lier-Titi",0,"yolo");
         Consumer u1= new Consumer("Lier-email1@email",100);
         Consumer u2= new Consumer("Lier-email2@email",50);
 
         //Recording users
-        JPA.em().persist(u1);
-        JPA.em().persist(u2);
+        consumers.insert(u1);
+        consumers.insert(u2);
 
         //Add the person and link it to an other user
         personDAO.add(p,u1.getEmail());
+        consumers.updateById("Lier-email1@email", u1);
 
         //Add the person and link it to an other user
-        Person pbd= (Person)JPA.em().createQuery("Select p FROM Person p WHERE p.name='Lier-Toto'").getSingleResult();
+        Person pbd= people.findOne(DBQuery.is("name", "Lier-Toto"));
         consumerDAO.linkUserPerson(pbd.getIdPerson(),u2.getEmail());
-
+        consumers.updateById("Lier-email2@email", u2);
+        
         //Check is both users have the same name
-        assertThat(u2.getPeople().get(0).getName()).isEqualTo(pbd.getName());
+        assertThat(u2.getPeople().get(0).fetch().getName()).isEqualTo(pbd.getName());
 
         //Clean
-        JPA.em().remove(u1);
-        JPA.em().remove(u2);
-        JPA.em().remove(pbd);
+        consumers.remove(u1);
+        consumers.remove(u2);
+        people.remove(pbd);
     }
-
-    @After
-    public void tearDown() {
-        JPA.em().getTransaction().commit();
-    }
-
+    
     @AfterClass
     public static void stopApp() {
-        JPA.bindForCurrentThread(null);
-        Helpers.stop(app);
+        //Helpers.stop(app);
     }
+
 }
--- test/unit/StatisticsTest.java
+++ test/unit/StatisticsTest.java
@@ -3,12 +3,17 @@ package unit;
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.inMemoryDatabase;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import models.Consumer;
 import models.Person;
 import models.Statistics;
+import net.vz.mongodb.jackson.DBQuery;
+import net.vz.mongodb.jackson.JacksonDBCollection;
 
+import org.bson.types.ObjectId;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -16,8 +21,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import play.db.jpa.JPA;
+import play.modules.mongodb.jackson.MongoDB;
 import play.test.FakeApplication;
 import play.test.Helpers;
+import dao.ConsumerDAO;
+import dao.ConsumerDAOImpl;
 import dao.PersonDAO;
 import dao.PersonDAOImpl;
 import dao.StatisticsDAO;
@@ -29,21 +37,25 @@ import dao.StatisticsDAOImpl;
  */
 public class StatisticsTest{
 
+	private PersonDAO personDAO = new PersonDAOImpl();
+    private ConsumerDAO consumerDAO = new ConsumerDAOImpl();
     private StatisticsDAO statisticsDAO = new StatisticsDAOImpl();
-    private PersonDAO personDAO = new PersonDAOImpl();
-
     private static FakeApplication app;
-
+    private static JacksonDBCollection<Consumer, String> consumers ;
+    private static JacksonDBCollection<Person, String> people;
+	private static JacksonDBCollection<Statistics, String> statistics;
+	
     @BeforeClass
     public static void startApp() {
-        app = Helpers.fakeApplication(inMemoryDatabase());
+    	Map<String, String> config = new HashMap<String, String>();
+        config.put("ehcacheplugin", "disabled");
+        config.put("mongodbJacksonMapperCloseOnStop", "disabled");
+        app = Helpers.fakeApplication(config);
         Helpers.start(app);
-        JPA.bindForCurrentThread( JPA.em("default"));
-    }
-
-    @Before
-    public void setUp() {
-        JPA.em().getTransaction().begin();
+        consumers = MongoDB.getCollection("Consumer", Consumer.class, String.class);
+    	people = MongoDB.getCollection("Person", Person.class, String.class);
+    	statistics = MongoDB.getCollection("Statistics", Statistics.class, String.class);
+        
     }
 
     
@@ -52,26 +64,19 @@ public class StatisticsTest{
      */
     @Test
     public void add() {
-    	Person p =new Person("pName", "pFirstname",0,"https://url");
+    	Person p =new Person(ObjectId.get().toString(),"pName", "pFirstname",0,"https://url");
         Consumer u= new Consumer("email@email",100);
-        JPA.em().persist(u);
+        consumers.insert(u);
         personDAO.add(p,u.getEmail());
         
         statisticsDAO.add(p.getIdPerson(), u.getEmail());
         statisticsDAO.add(p.getIdPerson(), u.getEmail());
         
-        List<Statistics> lp= JPA.em().createQuery("Select s FROM Statistics s WHERE s.person = :member",Statistics.class).setParameter("member", p).getResultList();
+        List<Statistics> lp= statistics.find(DBQuery.in("person.$id", p.getIdPerson())).toArray();
         assertThat(lp.size()==2);
     }
-
-    @After
-    public void tearDown() {
-        JPA.em().getTransaction().commit();
-    }
-
     @AfterClass
     public static void stopApp() {
-        JPA.bindForCurrentThread(null);
         Helpers.stop(app);
     }
 }
