e58f319af712469b00d26808e9ce91751a6319cd
safia <safiaboukhdimi@gmail.com>
2015-03-16 11:58:52 +0100

    remove unused import

--- app/dao/StatisticsDAOImpl.java
+++ app/dao/StatisticsDAOImpl.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Logger;
 
 import models.Consumer;
 import models.Person;
